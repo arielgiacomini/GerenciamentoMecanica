32527b0ff33fb4580ed1780ca34664bebd9204f8
Ariel Giacomini <ariel.giacomini@minutoseguros.com.br>
2020-04-02 01:29:55 -0300

    OrcamentoPeca e garantindo funcionamento OrcamentoMaodeObra, são 25 requisiçoes HTTP até o momento

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,38 +1,47 @@
 const express = require('express');
 
 const ClienteController = require('./controllers/ClienteController');
-const OrcamentoController = require('./controllers/OrcamentoController');
-const ColaboradorController = require('./controllers/ColaboradorController');
-const VeiculoController = require('./controllers/VeiculoController');
 const ClienteVeiculoController = require('./controllers/ClienteVeiculoController');
+const ColaboradorController = require('./controllers/ColaboradorController');
+const MaodeObraController = require('./controllers/MaodeObraController');
+const OrcamentoController = require('./controllers/OrcamentoController');
 const OrcamentoMaodeObraController = require('./controllers/OrcamentoMaodeObraController');
-
+const OrcamentoPecaController = require('./controllers/OrcamentoPecaController');
+const VeiculoController = require('./controllers/VeiculoController');
 
 const routes = express.Router();
+
+routes.get('/cliente', ClienteController.GetClientesForAll);
+routes.post('/cliente', ClienteController.CriarCliente);
+routes.delete('/cliente/:id', ClienteController.DeletarCliente);
+
+routes.get('/cliente-veiculo', ClienteVeiculoController.GetClienteVeiculoForAll);
+routes.post('/cliente-veiculo', ClienteVeiculoController.CriarClienteVeiculo);
+routes.delete('/cliente-veiculo/:id', ClienteVeiculoController.DeletarClienteVeiculo);
 
 routes.post('/colaborador/logOn', ColaboradorController.SessaoLogar);
 routes.post('/colaborador/', ColaboradorController.CriarColaborador);
 routes.get('/colaborador', ColaboradorController.GetColaboradorForAll);
 routes.delete('/colaborador/:id', ColaboradorController.DeletarColaborador);
 
-routes.get('/cliente', ClienteController.GetClientesForAll);
-routes.post('/cliente', ClienteController.CriarCliente);
-routes.delete('/cliente/:id', ClienteController.DeletarCliente);
+routes.get('/mao-de-obra', MaodeObraController.GetMaodeObraForAll);
+routes.post('/mao-de-obra', MaodeObraController.CriarMaodeObra);
+routes.delete('/mao-de-obra/:id', MaodeObraController.DeletarMaodeObra);
 
 routes.get('/orcamento', OrcamentoController.GetOrcamentoForAll);
 routes.post('/orcamento', OrcamentoController.CriarOrcamento);
 routes.delete('/orcamento/:id', OrcamentoController.DeletarOrcamento);
 
-routes.get('/veiculo', VeiculoController.GetVeiculoForAll);
-routes.post('/veiculo', VeiculoController.CriarVeiculo);
-routes.delete('/veiculo/:id', VeiculoController.DeletarVeiculo);
-
-routes.get('/cliente-veiculo', ClienteVeiculoController.GetClienteVeiculoForAll);
-routes.post('/cliente-veiculo', ClienteVeiculoController.CriarClienteVeiculo);
-routes.delete('/cliente-veiculo/:id', ClienteVeiculoController.DeletarClienteVeiculo);
-
 routes.get('/orcamento-mao-de-obra', OrcamentoMaodeObraController.GetOrcamentoMaodeObraForAll);
 routes.post('/orcamento-mao-de-obra', OrcamentoMaodeObraController.CriarOrcamentoMaodeObra);
 routes.delete('/orcamento-mao-de-obra/:id', OrcamentoMaodeObraController.DeletarOrcamentoMaodeObra);
+
+routes.get('/orcamento-peca', OrcamentoPecaController.GetOrcamentoPecaForAll);
+routes.post('/orcamento-peca', OrcamentoPecaController.CriarOrcamentoPeca);
+routes.delete('/orcamento-peca/:id', OrcamentoPecaController.DeletarOrcamentoPeca);
+
+routes.get('/veiculo', VeiculoController.GetVeiculoForAll);
+routes.post('/veiculo', VeiculoController.CriarVeiculo);
+routes.delete('/veiculo/:id', VeiculoController.DeletarVeiculo);
 
 module.exports = routes;
