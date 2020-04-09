57e7b8e6d71218bc197c5eb03a73099492b1607e
Ariel Giacomini <arieltecnologia@outlook.com>
2020-04-09 00:07:00 -0300

    Aplicativo já consegue ler a Api do backend

--- mobile/src/pages/Orcamento/index.js
+++ mobile/src/pages/Orcamento/index.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import { View, Image, Text, TouchableOpacity, FlatList } from 'react-native';
+
+import api from '../../services/api';
 
 import logoImg from '../../assets/SGM/logo.png';
 
 import styles from './styles';
 
 export default function Orcamento() {
+    const [orcamentos, setOrcamentos] = useState([]);
+    const [totalOrcamentos, setTotalOrcamentos] = useState([]);
     const navigation = useNavigation();
 
-    function navigateToDetail() {
-        navigation.navigate('OrcamentoDetalhes');
+    function navigateToDetail(orcamento) {
+        navigation.navigate('OrcamentoDetalhes', { orcamento });
     }
+
+    async function loadOrcamentos() {
+        const response = await api.get('orcamento');
+
+        setOrcamentos(response.data);
+        setTotalOrcamentos(response.headers['x-total-count']);
+    }
+
+    useEffect(() => {
+        loadOrcamentos();
+    }, []);
 
     return (
         <View style={styles.container}>
@@ -21,31 +36,32 @@ export default function Orcamento() {
             <Text style={styles.headerSlugNameProject}>Mecânica do Loquinho</Text>
         </View>
             <Text style={styles.headerText}>
-                Total de <Text style={styles.headerTextBold}>0</Text> orçamentos gerdos hoje.
+                Total de <Text style={styles.headerTextBold}>{totalOrcamentos}</Text> orçamentos gerdos hoje.
             </Text>
             
             <Text style={styles.title}>Bem-Vindo!</Text>
             <Text style={styles.subTitle}>Aqui você pode gerar um orçamento ou um serviço para seu cliente.</Text>
 
             <FlatList
-                data={[1, 2, 3, 4, 5, 6, 7]}
+                data={orcamentos}
                 style={styles.orcamentoList}
-                keyExtractor={orcamento => String(orcamento)}
+                keyExtractor={orcamentos => String(orcamentos.Id)}
                 showsVerticalScrollIndicator={false}
-                renderItem={() => (
+                renderItem={({ item: orcamento }) => (
                     <View style={styles.orcamento}>
-                    <Text style={styles.orcamentoProperty}>CLIENTE:</Text>
-                    <Text style={styles.orcamentoValue}>Ariel Giacomini</Text>
 
-                    <Text style={styles.orcamentoProperty}>ORCAMENTO:</Text>
-                    <Text style={styles.orcamentoValue}>137827482</Text>
+                    <Text style={styles.orcamentoProperty}>Nº:</Text>
+                    <Text style={styles.orcamentoValue}>{orcamento.OrcamentoId}</Text>
+
+                    <Text style={styles.orcamentoProperty}>Cliente:</Text>
+                    <Text style={styles.orcamentoValue}>{orcamento.Descricao}</Text>
 
                     <Text style={styles.orcamentoProperty}>VALOR TOTAL:</Text>
-                    <Text style={styles.orcamentoValue}>R$ 129,89</Text>
+                    <Text style={styles.orcamentoValue}>{Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'}).format(orcamento.ValorTotal)}</Text>
 
                     <TouchableOpacity 
                             style={styles.detailsButton} 
-                            onPress={navigateToDetail}
+                            onPress={() => navigateToDetail(orcamento)}
                     >
                         <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
                          <Feather name="arrow-right" size={16} color="#E02041" />
